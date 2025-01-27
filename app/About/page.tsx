d3ac8266ea05d21668da897f2fd1f9090a6edff5
unknown <obeddanny17@gmail.com>
2025-01-27 21:18:53 +0700

    about page

---
+++ app/About/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+const AboutPage = () => {
+
+
+  return (
+    <div>
+        <h1>Halaman about</h1>
+    </div>
+  )
+}
+export default AboutPage;
